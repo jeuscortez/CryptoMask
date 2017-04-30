3191aac343c56d6b3088bc2b53d73ad9b9abf697
jeuscortez <jic619@yahoo.com>
2017-04-29 17:52:11 -0700

    Added Java Doc for Class

--- app/src/main/java/com/example/cortez/cryptomask/AffineCipher.java
+++ app/src/main/java/com/example/cortez/cryptomask/AffineCipher.java
@@ -11,6 +11,11 @@ public class AffineCipher {
 
     AffineCipher(){}
 
+    /**
+     * This method is responsible for generating alpha and beta key for affine cipher
+     * It genrates two randoms and then tests if Alpha is relatively prime to 26
+     * If the alpha passes the test it creates the keys and stores them into an array
+     */
     protected static int[] keyGenerator(){
 
         Random rand = new Random();
@@ -29,11 +34,26 @@ public class AffineCipher {
 
     }
 
+    /**
+     * This method is responsible for checking if alpha and 26 are relatively prime
+     *It calls the gcd method to do the testing
+     *
+     * @param a is the alpha key generated that will be tested to be relatively prime
+     * @param m is the value of our modulo that needs to be relatively prime to alpha
+     * @return True if they are relatively prime, False if they are not
+     */
     private static boolean relativelyPrimeTest(int a, int m){
 
         return gcd(a, m)==1;
     }
 
+    /**
+     * This method is responsible for testing what the Greatest Common Denominator is of the two numbers
+     *
+     * @param a is the alpha key for the Affine Cipher
+     * @param m is the value of the modulo for the Affine Cipher
+     * @return 1 if they are relatively prime or other value which proves they are not relatively prime.
+     */
     private static int gcd(int a, int m){
         int t;
         while(m!=0){
@@ -44,6 +64,14 @@ public class AffineCipher {
         return a;
     }
 
+    /**
+     * This method is responsible for creating the encryption of the text, which is referred to as cipher text
+     *
+     * @param text is the users' selected text to encrypt
+     * @param Alpha is the alpha key generated and tested for validation
+     * @param Beta is the other key that was also generated with alpha key
+     * @return The Cipher text form of the regular text provided
+     */
     protected static String encryption(String text,int Alpha, int Beta){
         text=text.toLowerCase();
         StringBuilder CipherText = new StringBuilder();
@@ -62,6 +90,14 @@ public class AffineCipher {
         return CipherText.toString();
     }
 
+    /**
+     * This method is responsible for decrypting the Affine Cipher Encryption
+     *
+     * @param cipherText is the encrypted form of the users regular text
+     * @param Alpha is the key used to encrypt the user's text
+     * @param Beta is the other key used to encrypt user's text as well
+     * @return The Original Text that user chose to encrypt
+     */
     protected static String decryption(String cipherText,int Alpha, int Beta){
         StringBuilder plainText = new StringBuilder();
         BigInteger inverse = BigInteger.valueOf(Alpha).modInverse(BigInteger.valueOf(26));
